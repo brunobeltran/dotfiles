353a0ac1871b8c2c507e5fb65334fbae42f1646e
Bruno Beltran <bruno@digit.bio>
2023-12-04 07:22:32 -0500

    Formatting fixes in bash_profile and lspzero.lua.

--- dotfiles/config/nvim/lua/plugins/config/lspzero.lua
+++ dotfiles/config/nvim/lua/plugins/config/lspzero.lua
@@ -1,45 +1,46 @@
 local lsp = require("lsp-zero").preset({
-	manage_nvim_cmp = {
-		set_sources = "recommended"
-	}
+    manage_nvim_cmp = {
+        set_sources = "recommended"
+    }
 })
 
 lsp.on_attach(function(client, bufnr) -- NOQA
-	lsp.default_keymaps({ buffer = bufnr })
+    lsp.default_keymaps({ buffer = bufnr })
 end)
 
 lsp.ensure_installed({
-	"bashls",
-	"bufls",
-	-- "bzl",
-	"clangd",
-	"cssls",
-	"dockerls",
-	"html",
-	"jsonls",
-	"lua_ls",
-	"marksman",
-	"pyright",
-	"rust_analyzer",
-	"sqlls",
-	"tailwindcss",
-	"terraformls",
-	"tflint",
-	"texlab",
-	"vimls",
-	"volar",
-	"yamlls",
+    "bashls",
+    "bufls",
+    -- "bzl",
+    "clangd",
+    "cssls",
+    "dockerls",
+    "html",
+    "jsonls",
+    "lua_ls",
+    "marksman",
+    "pyright",
+    "rust_analyzer",
+    "sqlls",
+    "tailwindcss",
+    "taplo",
+    "terraformls",
+    "tflint",
+    "texlab",
+    "vimls",
+    "volar",
+    "yamlls",
 })
 
 lsp.format_on_save({
-	format_opts = {
-		async = false,
-		timeout_ms = 10000,
-	},
-	servers = {
-		['lua_ls'] = { 'lua' },
-		['rust_analyzer'] = { 'rust' },
-	}
+    format_opts = {
+        async = false,
+        timeout_ms = 10000,
+    },
+    servers = {
+        ['lua_ls'] = { 'lua' },
+        ['rust_analyzer'] = { 'rust' },
+    }
 })
 
 require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
