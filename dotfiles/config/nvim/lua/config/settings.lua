ef6fc39ff8a9d1dd8c06a42af1929aeec599a367
Bruno Beltran <bruno@digit.bio>
2024-12-27 10:44:54 -0600

    Auto-format lua config settings.lua

--- dotfiles/config/nvim/lua/config/settings.lua
+++ dotfiles/config/nvim/lua/config/settings.lua
@@ -5,6 +5,7 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.colorcolumn = "80"
+vim.opt.showtabline = 2
 
 vim.opt.swapfile = true
 vim.opt.backupdir = os.getenv("HOME") .. "/.vim/temp_dirs/nvim-backups"
@@ -13,6 +14,6 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/temp_dirs/nvim-undodir"
 vim.opt.undofile = true
 
 vim.g.python3_host_prog = os.getenv("HOME") .. "/.miniconda/envs/neovim/bin/python"
-vim.cmd [[
+vim.cmd([[
 let g:python_indent = {'disable_parentheses_indenting': v:false, 'closed_paren_align_last_line': v:false, 'searchpair_time out': 150, 'continue': 'shiftwidth() * 2', 'open_paren': 'shiftwidth()', 'nested_paren': 'shiftwidth()'}
-]]
+]])
