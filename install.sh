2c135c527db47db70bb052befa786138d5235650
Bruno Beltran <bruno@digit.bio>
2024-12-28 12:13:16 -0600

    Fix path post stow migration

--- install.sh
+++ install.sh
@@ -35,11 +35,15 @@ if ! which conda >/dev/null 2>&1; then
 fi
 
 # Many distros come with ancient fzf, install from source.
-if [[ ! -d "${BUILD_DIR}/fzf" ]]; then
-	git clone --depth 1 https://github.com/junegunn/fzf.git "${BUILD_DIR}/fzf"
+FZF_DIR="${BUILD_DIR}/fzf"
+if [[ ! -d "${FZF_DIR}" ]]; then
+	git clone --depth 1 https://github.com/junegunn/fzf.git "${FZF_DIR}"
 fi
-if ! which fzf >/dev/null 2>&1; then
-	"${BUILD_DIR}/fzf/install"
+# FZF comes with a very noisy install script that dumps stuff all over the
+# place, but one thing we can always be sure of is that once it is run, it will
+# download or build an FZF binary to this path.
+if [[ ! -f "${FZF_DIR}/bin/fzf" ]]; then
+	"${FZF_DIR}/install" --no-update-rc --key-bindings --completion --xdg
 fi
 
 # Install a nice shell prompt.
@@ -48,16 +52,21 @@ if ! which starship >/dev/null 2>&1; then
 fi
 
 # Build NeoVim from source.
-if [[ ! -d "${BUILD_DIR}/neovim" ]]; then
-	git clone https://github.com/neovim/neovim.git "${BUILD_DIR}/neovim"
+NEOVIM_SOURCE_DIR="${BUILD_DIR}/neovim-src"
+NEOVIM_BUILD_DIR="${BUILD_DIR}/neovim-build"
+if [[ ! -d "${NEOVIM_SOURCE_DIR}" ]]; then
+	git clone https://github.com/neovim/neovim.git "${NEOVIM_SOURCE_DIR}"
 fi
-if ! which nvim >/dev/null 2>&1; then
-	cd ./neovim
-	sudo mkdir -p /opt/nvim
-	sudo chown "${USER}" /opt/nvim
-	make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=/opt/nvim
-	make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=/opt/nvim install
-	cd ..
+if [[ ! -f "${NEOVIM_BUILD_DIR}/bin/nvim" ]]; then
+	{
+		cd "${NEOVIM_SOURCE_DIR}"
+		mkdir -p "${NEOVIM_BUILD_DIR}"
+		make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="${NEOVIM_BUILD_DIR}"
+		make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="${NEOVIM_BUILD_DIR}" install
+	}
 fi
+
+# Render the current build path into a file for use in `.set_path`.
+echo "${BUILD_DIR}" >"${SCRIPT_DIR}/dotfiles/dot-dotfiles-build-path"
 
 stow --dir "${SCRIPT_DIR}" --target="${HOME}" --adopt --dotfiles dotfiles
