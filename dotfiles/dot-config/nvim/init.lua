7754c3dfa193932b6451ff82eccaf9fffa4c99f3
Bruno Beltran <brunobeltran0@gmail.com>
2026-07-13 05:09:20 -0700

    Fix on_yank for nvim 0.13

--- dotfiles/dot-config/nvim/init.lua
+++ dotfiles/dot-config/nvim/init.lua
@@ -188,12 +188,12 @@ vim.keymap.set('n', '<leader>fll', ':set paste<CR>A  # NOQA: E501<ESC>:set nopas
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.hl.hl_op()`
+--  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.hl.hl_op()
+    vim.hl.on_yank()
   end,
 })
 
